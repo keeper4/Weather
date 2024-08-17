1a871d1b57ccf2f053545186f7bc39de1496e6a6
Oleksandr Chyzh <oleksandr.chyzh@soft-loft.eu>
2024-08-17 16:39:25 +0300

    update

--- weather/Base/Model/DifferenceCheckable.swift
+++ weather/Base/Model/DifferenceCheckable.swift
@@ -9,23 +9,23 @@
 import Foundation
 
 protocol DifferenceCheckable {
-
+    
     func isContained(in otherDict: Self) -> Bool
 }
 
 extension DifferenceCheckable where Self == [String : Any] {
-
+    
     func isContained(in otherDict: Self) -> Bool {
-        //if self.count > otherDict.count {
+        
         if self.count != otherDict.count {
             return false
         }
-
+        
         for (key, value) in self {
             if let otherValue = otherDict[key] {
                 if let value = value as? Self,
-                    let otherValue = otherValue as? Self,
-                    !value.isContained(in: otherValue)
+                   let otherValue = otherValue as? Self,
+                   !value.isContained(in: otherValue)
                 {
                     return false
                 } else if !compare(value, to: otherValue) {
@@ -35,7 +35,7 @@ extension DifferenceCheckable where Self == [String : Any] {
                 return false
             }
         }
-
+        
         return true
     }
 }
@@ -45,95 +45,79 @@ extension Dictionary: DifferenceCheckable where Key == String, Value == Any {}
 private func compare(_ one: Any, to another: Any) -> Bool {
     switch (one, another) {
     case let (one       as [String : Any],
-              another   as [String : Any]):
-        return one == another
-
-    case let (one       as [Any],
-              another   as [Any]):
-        return one == another
-
+        another   as [String : Any]):
+        return NSDictionary(dictionary: one).isEqual(another)
+        //return one == another
+        
+            case let (one       as [Any],
+                      another   as [Any]):
+        return compareAnyArray(one, to: another)
+        
     case let (one       as Bool,
               another   as Bool):
         return one == another
-
+        
     case let (one       as Int,
               another   as Int):
         return one == another
-
+        
     case let (one       as Float,
               another   as Float):
         return one == another
-
+        
     case let (one       as Double,
               another   as Double):
         return one == another
-
+        
     case let (one       as String,
               another   as String):
         return one == another
-
+        
     case let (one       as NSNumber,
               another   as NSNumber):
         return one == another
-
+        
     case let (one       as BaseModel,
               another   as BaseModel):
-        return one.toJSON() == another.toJSON()
-
+        return NSDictionary(dictionary: one.toJSON()).isEqual(another.toJSON())
+        
     case let (one       as NSObject,
               another   as NSObject):
         return one.isEqual(another)
-
+        
     case let (one       as AnyObject,
               another   as AnyObject):
         return one === another
-
+        
     default:
         assert(false, "comparing unsupported types")
         return false
     }
 }
 
-extension Equatable where Self == [String : Any] {
+private func compareAnyArray(_ one: [Any], to another: [Any]) -> Bool {
+    // Check if the arrays have the same number of elements
+    guard one.count == another.count else {
+        return false
+    }
 
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        if lhs.count != rhs.count {
-            return false
-        }
-        for (lKey, lValue) in lhs {
-            if let rValue = rhs[lKey] {
-                if !compare(lValue, to: rValue) {
-                    return false
-                }
-            } else {
+    // Iterate through the elements of the arrays and compare them
+    for (element1, element2) in zip(one, another) {
+        // Use optional casting to compare elements of the same type
+        if let value1 = element1 as? Int, let value2 = element2 as? Int {
+            if value1 != value2 {
                 return false
             }
-        }
-        return true
-    }
-
-    static func != (lhs: Self, rhs: Self) -> Bool {
-        return !(lhs == rhs)
-    }
-}
-
-extension Equatable where Self == [Any] {
-
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        if lhs.count != rhs.count {
-            return false
-        }
-
-        for (index, lValue) in lhs.enumerated() {
-            let rValue = rhs[index]
-            if !compare(lValue, to: rValue) {
+        } else if let str1 = element1 as? String, let str2 = element2 as? String {
+            if str1 != str2 {
                 return false
             }
+        } else {
+            // Handle other types or return false if they cannot be compared
+            return false
         }
-        return true
     }
 
-    static func != (lhs: Self, rhs: Self) -> Bool {
-        return !(lhs == rhs)
-    }
+    // All elements match, return true
+    return true
 }
